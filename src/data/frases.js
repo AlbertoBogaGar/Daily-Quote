3f837de329a824ce385f3100d94fabf7dbd5eaf2
Alberto Boga <alberto.boggar@educa.jcyl.es>
2024-08-26 16:55:18 +0200

    Prueba 1

---
+++ src/data/frases.js
@@ -0,0 +1,8 @@
+export const phrases = [
+    "La vida es 10% lo que me ocurre y 90% cómo reacciono a ello.",
+    "El único modo de hacer un gran trabajo es amar lo que haces.",
+    "No cuentes los días, haz que los días cuenten.",
+    "La mejor forma de predecir el futuro es inventarlo.",
+    "No te preocupes por fracasar, solo tienes que estar en lo cierto una sola vez."
+  ];
+  
