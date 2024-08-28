3e50020934576c57922de6fccaaa9ccf6bc0be48
Alberto Boga <alberto.boggar@educa.jcyl.es>
2024-08-28 17:30:25 +0200

    Añado apartado de newsletter

--- src/data/frases.js
+++ src/data/frases.js
@@ -1,5 +1,5 @@
 export const phrases = [
-    "Cuando seas feliz por nada, solo enotnces el mundo te lo dará todo. Y como ya habrás aprendido a no necesitar nada de nada para disfrutar de la vida, no te apegarás a tu fortuna. Sabrás que te basta con sonreir y amar a la gente para seguir siendo rico sin limitación.",
+    "Cuando seas feliz por nada, solo entonces el mundo te lo dará todo. Y como ya habrás aprendido a no necesitar nada de nada para disfrutar de la vida, no te apegarás a tu fortuna. Sabrás que te basta con sonreir y amar a la gente para seguir siendo rico sin limitación.",
     "500 kg de estiércol te están esperando donde decidas ir.",
     "No cuentes los días, haz que los días cuenten.",
     "La mejor forma de predecir el futuro es inventarlo.",
