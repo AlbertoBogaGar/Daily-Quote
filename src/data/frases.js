fd91d61b6f925f733e6042d847d0a5ef0cc639f6
Alberto Boga <alberto.boggar@educa.jcyl.es>
2024-08-30 17:01:56 +0200

    Prueba

--- src/data/frases.js
+++ src/data/frases.js
@@ -3,13 +3,13 @@ export const phrases = [
     "500 kg de estiércol te están esperando donde decidas ir.",
     "El mejor momento para plantar un árbol fue hace 20 años. El segundo mejor momento es hoy.",
     "Los problemas de dinero nunca se han solucionado con dinero, sino con ideas nuevas.",
-    "Puedes crear software y medios que operen por ti mientras duermes.",
-    "Las personas muertas reciben más flores que las vivas porque el arrepentimiento es más fuerte que la gratitud.",
+   "Puedes crear software y medios que operen por ti mientras duermes.",
+   /* "Las personas muertas reciben más flores que las vivas porque el arrepentimiento es más fuerte que la gratitud.",
     "El sordo cree los que bailan están locos.",
     "Un pájaro posado en un árbol nunca tiene miedo de que la rama se rompa, porque su confianza no está en la rama, sino en sus propias alas.",
     "La suerte toma a menudo la forma de una oportunidad, pero pocas veces nos viene de otro modo.",
     "Si fallas en planear, en realidad estás planeando fallar.",
     "Si haces lo que pocos están dispuestos a hacer durante un tiempo, podrás disfrutar lo que muy pocos podrán hacer durante mucho tiempo.",
-    "El consumidor vota cada día con su compra y no solo cada cuatro años.",
+    "El consumidor vota cada día con su compra y no solo cada cuatro años.",*/
   ];
   
