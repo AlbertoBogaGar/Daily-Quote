e9097df54ef7840b0228481fb8cae84111616d05
Alberto Boga <alberto.boggar@educa.jcyl.es>
2024-08-31 18:10:00 +0200

    Actualizo frases

--- src/data/frases.js
+++ src/data/frases.js
@@ -1,15 +1,15 @@
 export const phrases = [
-    "Cuando seas feliz por nada, solo entonces el mundo te lo dará todo. Y como ya habrás aprendido a no necesitar nada de nada para disfrutar de la vida, no te apegarás a tu fortuna. Sabrás que te basta con sonreir y amar a la gente para seguir siendo rico sin limitación.",
-    "500 kg de estiércol te están esperando donde decidas ir.",
-    "El mejor momento para plantar un árbol fue hace 20 años. El segundo mejor momento es hoy.",
-    "Los problemas de dinero nunca se han solucionado con dinero, sino con ideas nuevas.",
-   "Puedes crear software y medios que operen por ti mientras duermes.",
-   /* "Las personas muertas reciben más flores que las vivas porque el arrepentimiento es más fuerte que la gratitud.",
-    "El sordo cree los que bailan están locos.",
-    "Un pájaro posado en un árbol nunca tiene miedo de que la rama se rompa, porque su confianza no está en la rama, sino en sus propias alas.",
-    "La suerte toma a menudo la forma de una oportunidad, pero pocas veces nos viene de otro modo.",
-    "Si fallas en planear, en realidad estás planeando fallar.",
-    "Si haces lo que pocos están dispuestos a hacer durante un tiempo, podrás disfrutar lo que muy pocos podrán hacer durante mucho tiempo.",
-    "El consumidor vota cada día con su compra y no solo cada cuatro años.",*/
+    "Cuando seas feliz por nada, solo entonces el mundo te lo dará todo. Y como ya habrás aprendido a no necesitar nada de nada para disfrutar de la vida, no te apegarás a tu fortuna. Sabrás que te basta con sonreir y amar a la gente para seguir siendo rico sin limitación. - Raimón Samsó",
+    "500 kg de estiércol te están esperando donde decidas ir. - Mark Manson",
+    "El mejor momento para plantar un árbol fue hace 20 años. El segundo mejor momento es hoy. - Proverbio chino",
+    "Los problemas de dinero nunca se han solucionado con dinero, sino con ideas nuevas. - Sergio Fernández",
+   "Puedes crear software y medios que operen por ti mientras duermes. - Naval Ravikant",
+   "Las personas muertas reciben más flores que las vivas porque el arrepentimiento es más fuerte que la gratitud. - Anne Frank",
+    "El sordo cree los que bailan están locos. - Proverbio hindú",
+    "Un pájaro posado en un árbol nunca tiene miedo de que la rama se rompa, porque su confianza no está en la rama, sino en sus propias alas. ",
+    "La suerte toma a menudo la forma de una oportunidad, pero pocas veces nos viene de otro modo. - Mark Twain",
+    "Si fallas en planear, en realidad estás planeando fallar. - Benjamin Franklin",
+    "Si haces lo que pocos están dispuestos a hacer durante un tiempo, podrás disfrutar lo que muy pocos podrán hacer durante mucho tiempo. - Mark Manson",
+    "El consumidor vota cada día con su compra y no solo cada cuatro años. - Joan Melé",
   ];
   
