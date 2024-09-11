eb8fdc598c8af7cdb7baa64efe58d1fa63792dc3
Alberto Boga <alberto.boggar@educa.jcyl.es>
2024-09-11 15:15:56 +0200

    Añado frases

--- src/data/frases.js
+++ src/data/frases.js
@@ -5,7 +5,7 @@ export const phrases = [
     "Los problemas de dinero nunca se han solucionado con dinero, sino con ideas nuevas - Sergio Fernández.",
    "Puedes crear software y medios que operen por ti mientras duermes.- Naval Ravikant.",
    "Las personas muertas reciben más flores que las vivas porque el arrepentimiento es más fuerte que la gratitud - Anne Frank.",
-    "El sordo cree los que bailan están locos - Proverbio hindú.",
+    "El sordo cree que los que bailan están locos - Proverbio hindú.",
     "Un pájaro posado en un árbol nunca tiene miedo de que la rama se rompa, porque su confianza no está en la rama, sino en sus propias alas. ",
     "La suerte toma a menudo la forma de una oportunidad, pero pocas veces nos viene de otro modo - Mark Twain.",
     "Si fallas en planear, en realidad estás planeando fallar - Benjamin Franklin.",
